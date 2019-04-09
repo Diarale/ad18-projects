164a4ce941498d16b31578dc3544c3bc348e43b6
Jasmin Raudsepp <43493606+Diarale@users.noreply.github.com>
2019-04-09 11:09:52 -0700

    Cube Homework (I Made Five Cube One Click)
    (YOU Click YOU Cube)

---
+++ stuff/Assets/coll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coll : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0)) 
+        {
+        for (int i = 0; i <5 ; i++)
+        {
+            Instantiate(prefab, new Vector3(i * 2.0F, 0, 0), Quaternion.identity);
+        }
+        }
+    }
+}
